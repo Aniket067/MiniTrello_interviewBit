0da9790f043f1ff538d135a82554730c5909df0b
Aniket Gupta <aniketgupta2405@gmail.com>
2025-09-14 17:13:10 +0530

    first commit

---
+++ pages/error.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <h1>
+     Some Error Occured
+      <span role="img" aria-label="pray">
+        
+      </span>
+    </h1>
+  );
+};
+
+export default Error;
